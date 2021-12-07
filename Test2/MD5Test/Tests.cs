8ca26d32e16fe8c46941e8778bdfc6bb166d1563
AndreiZaycev <Mikowftv30@mail.ru>
2021-12-07 17:00:30 +0300

    Test done

---
+++ Test2/MD5Test/Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MD5Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestThatTheTwoProgramsAreWorkingCorrectly()
+        {
+            var md5Hash = System.Security.Cryptography.MD5.Create();
+            var calculator = new Hasher(md5Hash);
+            firstTimer.Start();
+            calculator.CalculatePath(inputPath, false);
+            firstTimer.Stop();
+            var firstTime = firstTimer.ElapsedMilliseconds;
+            secondTimer.Start();
+            calculator.CalculatePath(inputPath, true);
+            secondTimer.Stop();
+        }
+    }
+}
