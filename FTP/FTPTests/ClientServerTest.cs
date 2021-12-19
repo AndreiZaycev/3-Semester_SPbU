e6513ea250a6723bfb8b10d2cd9a401c5e77bef4
AndreiZaycev <71140884+AndreiZaycev@users.noreply.github.com>
2021-12-19 17:15:06 +0300

    Update ClientServerTest.cs

--- FTP/FTPTests/ClientServerTest.cs
+++ FTP/FTPTests/ClientServerTest.cs
@@ -27,11 +27,10 @@ public class Tests
     public async Task ListShouldReturnRightSizeAndItems()
     {
         var actual = await _client.List(_path);
-        Assert.AreEqual(3, actual.Count);
+        Assert.AreEqual(2, actual.Count);
         var expected = new[]
         {
             ($"{_path}/Test", true),
-            ($"{_path}/test3.txt", false),
             ($"{_path}/test1.txt", false)
         };
 
