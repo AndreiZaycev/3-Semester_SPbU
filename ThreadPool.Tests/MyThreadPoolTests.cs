54ec404ffee3e20c9cbac31dcd62a2271922ebd6
AndreiZaycev <Mikowftv30@mail.ru>
2022-04-03 19:04:14 +0300

    2 test

--- ThreadPool.Tests/MyThreadPoolTests.cs
+++ ThreadPool.Tests/MyThreadPoolTests.cs
@@ -33,5 +33,17 @@ public class MyThreadPoolTests
             Assert.IsTrue(task.IsCompleted);
         }
     }
+
+    [Test]
+    public void AggregateExceptionTest()
+    {
+        var pool = new MyThreadPool(10);
+        var task1 = pool.Submit(() => 0);
+        var task2 = task1.ContinueWith(j => 1 / j);
+        var task3 = task2.ContinueWith(j => j.ToString());
+
+        Assert.Throws<AggregateException>(() => _ = task2.Result);
+        Assert.Throws<AggregateException>(() => _ = task3.Result);
+    }
     
 }
