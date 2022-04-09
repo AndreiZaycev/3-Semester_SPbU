c85c5aa86644c2398442cae0ada976838bc184ec
AndreiZaycev <Mikowftv30@mail.ru>
2022-04-09 20:46:10 +0300

    wip

--- ThreadPool.Tests/MyThreadPoolTests.cs
+++ ThreadPool.Tests/MyThreadPoolTests.cs
@@ -74,49 +74,5 @@ public class MyThreadPoolTests
             Assert.Throws<ThreadPoolShutdownException>(() => _tasks[i].ContinueWith(_ => 0));
         }
     }
-
-    [Test]
-    public void ContinueWithShouldCalculateTasks()
-    {
-        var continueTasks = new IMyTask<int>[CountOfTasks];
-        for (var i = 0; i < CountOfTasks; i++)
-        {
-            continueTasks[i] = _tasks[i].ContinueWith(x => x + 1);
-        }
-
-        for (var i = 0; i < CountOfTasks; i++)
-        {
-            Assert.AreEqual(i + 1, continueTasks[i].Result);
-        }
-    }
-
-    [Test]
-    public void ContinueWithShouldCompleteTheAssignedTasks()
-    {
-        var continueTasks = new IMyTask<int>[CountOfTasks];
-        for (var i = 0; i < CountOfTasks; i++)
-        {
-            continueTasks[i] = _tasks[i].ContinueWith(x => x * x);
-        }
-
-        _threadPool.Shutdown();
-
-        for (var i = 0; i < CountOfTasks; i++)
-        {
-            Assert.AreEqual(i * i, continueTasks[i].Result);
-        }
-    }
-
-    [Test]
-    public void ThreadPoolShouldRaiseExceptionWhenNumberOfThreadsLessOrEqualToZero()
-    {
-        Assert.Throws<ArgumentOutOfRangeException>(() =>
-        {
-            var _ = new MyThreadPool(-228);
-        });
-        Assert.Throws<ArgumentOutOfRangeException>(() =>
-        {
-            var _ = new MyThreadPool(0);
-        });
-    }
+    
 }
